0ae0dff2ed903f2cb51b4a834c704ccc6334b200
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-05-26 23:38:58 -0400

    cognito

--- cognito.tf
+++ cognito.tf
@@ -23,7 +23,7 @@ resource "aws_cognito_user_pool_client" "client" {
   user_pool_id                         = aws_cognito_user_pool.main.id
   allowed_oauth_flows                  = ["code", "implicit"]
   allowed_oauth_scopes                 = ["email", "openid"]
-  callback_urls                        = ["https://creeperkeeper.com/console/", "http://localhost:5173/console/"]
+  callback_urls                        = ["https://creeperkeeper.com/home/", "http://localhost:5173/home/"]
   logout_urls                          = ["https://creeperkeeper.com/", "http://localhost:5173/"]
   allowed_oauth_flows_user_pool_client = true
   generate_secret                      = false
