a195229c7531edc77edfcbaca9a4aae1dc45435f
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-10-27 12:47:18 -0400

    saving

--- s3.tf
+++ s3.tf
@@ -1,3 +1,3 @@
-resource "aws_s3_bucket" "creeper_keeper" {
-  bucket = local.app_name
-}
+# resource "aws_s3_bucket" "creeper_keeper" {
+#   bucket = local.app_name
+# }
--- vars.tf
+++ vars.tf
@@ -1,21 +1,26 @@
-locals {
-  domain_uri       = "creeperkeeper"
-  domain_name      = "${local.domain_uri}.com"
+# locals {
+#   domain_uri       = "creeperkeeper"
+#   domain_name      = "${local.domain_uri}.com"
 
-  statemanager_api_subdomain = "statemanager"
-  statemanager_api_domain_name = "${local.statemanager_api_subdomain}.${local.domain_name}"
+#   statemanager_api_subdomain = "statemanager"
+#   statemanager_api_domain_name = "${local.statemanager_api_subdomain}.${local.domain_name}"
   
-  app_name = "creeper-keeper"
+#   app_name = "creeper-keeper"
 
-  ec2_app_name = "ec2-statemanager"
-  jwt_audience = ["ec2-instance-manager-api-resource"]
-  jwt_issuer = "https://${var.environment}-bxn245l6be2yzhil.us.auth0.com/"
+#   ec2_app_name = "ec2-statemanager"
+#   jwt_audience = ["ec2-instance-manager-api-resource"]
+#   jwt_issuer = "https://${var.environment}-bxn245l6be2yzhil.us.auth0.com/"
 
-  ec2_subdomain = "statemanager"
-  ec2_domain_name = "${local.ec2_subdomain}.creeperkeeper.com"
-}
+#   ec2_subdomain = "statemanager"
+#   ec2_domain_name = "${local.ec2_subdomain}.creeperkeeper.com"
+# }
 
-variable "environment" {
-  type = string
-  default = "dev"
-}
+# variable "environment" {
+#   type = string
+#   default = "dev"
+# }
+
+# variable "home_ip" {
+#   type = string
+#   sensitive = true
+# }
